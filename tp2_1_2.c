4dd5cb9f362e6c1e6eb9f0687ef04ebe816a9cd9
Sofia Cruz <sofiaacruz187@gmail.com>
2023-03-24 12:31:19 -0300

    Punto-2-d

---
+++ tp2_1_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 20
+
+int main() {
+    int i;
+    double vt[N], *punt;
+    punt = vt;
+
+    srand(time(NULL));
+
+    for (i = 0; i < N; i++) {
+        *(punt + i) = 1 + rand() % 100;
+        printf("%.2f ", *(punt + i));
+    }
+
+    getchar();
+    return 0;
+}
